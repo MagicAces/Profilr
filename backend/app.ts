24fb6821c4c082993c340e1406c00d561a34ddb0
MagicAces <isaac.oniti14@gmail.com>
2024-06-23 16:03:09 +0000

    deploying

--- backend/app.ts
+++ backend/app.ts
@@ -25,7 +25,12 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(
   session({
     secret: process.env.SECRET || "",
